bb62615f74c872bc52164468ddcfc04047af2d00
Alang Mahendra <alangmahendra@gmail.com>
2018-08-31 10:45:57 +0700

    delete env

--- controllers/EmailServices.js
+++ controllers/EmailServices.js
@@ -12,33 +12,59 @@ class EmailServices {
         const data = req.body
         // console.log('ini data biasa', data, 'ini stringify', JSON.stringify(data))
         const msg = {
-            to: ['javsss3@gmail.com','sigitsetiadi@gmail.com'],
+            to: ['javsss3@gmail.com', 'sigitsetiadi@gmail.com', 'alangmahendra@gmail.com'],
             from: 'info@kemodijakarta.com',
             subject: 'SESEORANG MENDAFTAR DI KEMODIJAKARTA',
             text: 'hello world',
-        html: `<div>
-        nama depan:<srtong>${data.values.namaDepan}</strong><br/>
-        nama belakang:<srtong>${data.values.namaBelakang}</strong><br/>
-        email:<srtong>${data.values.email}</strong><br/>
-        agama:<srtong>${data.values.agama}</strong><br/>
-        aliran agama:<srtong>${data.values.aliran}</strong><br/>
-        jenis kelamin:<srtong>${data.values.sex}</strong><br/>
-        alamat:<srtong>${data.values.alamat}</strong><br/>
-        nomor telepon:<srtong>${data.values.noTelp}</strong><br/>
-        nomor telepon darurat:<srtong>${data.values.noTelpDarurat}</strong><br/>
-        asuransi:<srtong>${data.values.Asuransi}</strong><br/>
-        kondisi kesehatan:<srtong>${data.values.kondisi}</strong><br/>
-        vegetarian:<srtong>${data.values.diet}</strong><br/>
-        minum suplement:<srtong>${data.values.suplemen}</strong><br/>
-        jenis penginapan:<srtong>${data.values.homestay}</strong><br/>
-        fasiltas penginapan:<srtong>${data.values.fasilitas}</strong><br/>
-        layanan kebersihan:<srtong>${data.values.kebersihan}</strong><br/>
-        transportasi:<srtong>${data.values.transportasi}</strong><br/>
-        butuh kunjungan keluarga:<srtong>${data.values.kunjungan}</strong><br/>
-        butuh konseling/siraman rohani sesuai agama dan aliran:<srtong>${data.values.konseling}</strong><br/>
-        hiburan:<srtong>${data.values.hiburan}</strong><br/>
-        rumah sakit tempat terapi:<srtong>${data.values.rumahSakit}</strong><br/>
-        budget yang dimiliki:<srtong>${data.values.budget}</strong><br/>
+            html: `<div>
+        <div>
+        <label>informasi pribadi pasien</label>
+        nama depan:<strong>${data.values.namaDepan}</strong><br/>
+        nama belakang:<strong>${data.values.namaBelakang}</strong><br/>
+        email:<strong>${data.values.email}</strong><br/>
+        agama:<strong>${data.values.agama}</strong><br/>
+        aliran agama:<strong>${data.values.aliran}</strong><br/>
+        jenis kelamin:<strong>${data.values.sex}</strong><br/>
+        alamat:<strong>${data.values.alamat}</strong><br/>
+        nomor telepon:<strong>${data.values.noTelp}</strong><br/>
+        <div>
+        Kontak Darurat Kerabat : <strong>${data.values.namaContactDarurat}</strong>
+        Hubungan Kerabat : <strong>${data.values.hubungan}</strong>
+        nomor telepon darurat:<strong>${data.values.noTelpDarurat}</strong><br/>
+        </div>
+        asuransi:<strong>${data.values.Asuransi}</strong><br/>
+        </div>
+        <br/>
+
+       <div>
+       <label>kondisi kesehatan</label
+       Jenis kanker:<strong>${data.values.jenisKanker}</strong><br/>
+       Stadium:<strong>${data.values.stadium}</strong><br/>
+       </div><br/>
+        
+        <div>
+        <label>jasa dan layanan perjalanan medis</label>
+        jenis penginapan:<strong>${data.values.homestay}</strong><br/>
+        <div>
+            <label>fasilitas yang diinginkan pasien</label>
+            kamar mandi dalam:<strong>${data.values.kamarMandiDalam}</strong><br/>
+            tv:<strong>${data.values.Tv}</strong><br/>
+        </div>
+        butuh kunjungan keluarga:<strong>${data.values.kunjungan}</strong><br/>
+        butuh konseling/siraman rohani sesuai agama dan aliran:<strong>${data.values.konseling}</strong><br/>
+        hiburan:<strong>${data.values.hiburan}</strong><br/>
+        kebutuhan lain:<strong>${data.values.lainLain}</strong>
+        </div><br>
+
+        <div>
+        berapa hari terapi:<strong>${data.values.jadwalTreatment}</strong><br/>
+        tanggal mulai Terapi:<strong>${data.values.tanggalMulai}</strong><br/>
+        tanggal selesai Terapi:<strong>${data.values.tanggalSelesai}</strong><br/>
+        kota asal:<strong>${data.values.asalKota}</strong><br/>
+        datang dengan metode tranportasi:<strong>${data.values.datangDengan}</strong><br/>
+        rumah sakit tempat terapi:<strong>${data.values.rumahSakit}</strong><br/>
+        budget yang dimiliki:<strong>${data.values.budget}</strong><br/>
+        </div>
         <br>dari alang mahendra,developer.
         </div>`
         }
