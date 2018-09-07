0b1887aaf5cd26e1ee3608e2bcc27783c69c0357
Alangmahendra <alangmahendra@gmail.com>
2018-09-07 10:43:52 +0700

    open buku tamu

--- routes/BukuTamu.js
+++ routes/BukuTamu.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const BukuTamu = require('../controllers/BukuTamu')
 const auth = require('../auth/auth')
-    router.get('/list',auth.isLogin,auth.isAdmin,BukuTamu.findAll)
+    router.get('/list',BukuTamu.findAll)
     router.post('/add',BukuTamu.add)
 
 module.exports = router
